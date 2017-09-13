10ba368251355f01d568fc16c827bd431bc589fa
Lasse Have Nielsen <lasse@havenielsen.dk>
2017-09-13 18:27:18 +0200

    Minor change

--- controllers/humidityController.js
+++ controllers/humidityController.js
@@ -1,15 +1,13 @@
 const constants = require(".././config/constants");
 const tempHumidSensor = require(".././utils/tempHumidSensor");
-module.exports = function(app, auth){
-  
-    app.get(constants.apiPath + '/humidity/value', function(request, response){
-        var currentTime = new Date();
-        var humidity = tempHumidSensor.readHumid();
 
-        res.json({
-          currentTime: currentTime, 
-          temperature: temperature
-        });
-    })
-  
-  }
+
+exports.getValue = function(req, res) {
+  var currentTime = new Date();
+  var humidity = tempHumidSensor.readHumid();
+
+  res.json({
+      currentTime: currentTime, 
+      humidity: humidity
+    });
+};
