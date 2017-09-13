29121c57899eecb5a38adf926cbd00561989bac5
Lasse Have Nielsen <lasse@havenielsen.dk>
2017-09-13 17:18:57 +0200

    more test

--- controllers/temperatureController.js
+++ controllers/temperatureController.js
@@ -5,8 +5,9 @@ module.exports = {
     // Edit an article
     getTempValue : function(request, response){
         var currentTime = new Date();
-        var temperature = tempHumidSensor.readTemp();
-
+        var temperature = tempHumidSensor.readTemp().ToFixed(2);
+        console.log(currentTime);
+        console.log(temperature);
         return ({
             currentTime: currentTime, 
             temperature: temperature
