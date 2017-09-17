2d4e3c9fc503c6f53bb31643982ca4fafe615c4d
Michał Kapiczyński <mix.kap@gmail.com>
2017-09-17 15:40:15 +0200

    Light LED added logging.

--- utils/lightLED.js
+++ utils/lightLED.js
@@ -4,11 +4,12 @@ exports.toggleLED = function (){
   var Gpio = onoff.Gpio, 
   led = new Gpio(4, 'out');
   var value = led.readSync()
+
   led.write(!value, function(){
+    console.log("Changed LED state to: " + !value);
   });
 
   process.on('SIGINT', function () {
-  clearInterval(interval);
   led.writeSync(0);
   led.unexport();
   console.log('Bye, bye!');
