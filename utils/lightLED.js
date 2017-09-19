32e995c51904aca67e937418ad08f322800d6518
Michał Kapiczyński <mix.kap@gmail.com>
2017-09-19 21:18:48 +0200

    [Bug-fix] Led turning off works now. Moved the initialization of Gpio outside the toggleLED function because it didn't manage to get hold of the PIN and always returned 0.

--- utils/lightLED.js
+++ utils/lightLED.js
@@ -1,23 +1,19 @@
 var onoff = require("onoff");
 
+var Gpio = onoff.Gpio,
+  led = new Gpio(4, "out");
+
 exports.toggleLED = function() {
-  var Gpio = onoff.Gpio,
-    led = new Gpio(4, "out");
-    console.log(led.readSync())
   var value = (led.readSync() + 1) % 2;
-
-  led.writeSync(value, function() {
+  
+  led.write(value, function() {
     console.log("Changed LED state to: " + value);
-
   });
 
   process.on("SIGINT", function() {
-    clearInterval();
     led.writeSync(0);
     led.unexport();
     console.log("Bye, bye!");
     process.exit();
   });
-
-  console.log("LED toggled");
 };
